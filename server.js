f4bbc323bc9c6541832917bf6fab31d7f081d35f
wwweert123 <puachengxuan.fp@gmail.com>
2024-09-28 05:18:11 +0800

    Create base paths

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ connectDB();
 // Cross Origin Resource Sharing
 app.use(cors());
 
+// built-in middleware for json (I only need JSON)
+app.use(express.json());
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
+});
+
 mongoose.connection.once("open", () => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
