afbb4985cdb90eee9a19f30b6de02859524bf4b8
wwweert123 <puachengxuan.fp@gmail.com>
2024-09-28 05:13:44 +0800

    Add schemas in mongoose

---
+++ Model/Cafe.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// UUID validation regex for version 4 UUIDs
+const uuidValidator =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+const cafeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    logo: {
+        type: String, // Optional field, can store logo image URL or path
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    id: {
+        type: String,
+        required: true,
+        unique: true, // Ensures the UUID is unique
+        match: uuidValidator, // Basic email validation regex
+    },
+});
+
+module.exports = mongoose.model("Cafe", cafeSchema);
---
+++ Model/Employee.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        match: /^UI[A-Za-z0-9]{7}$/, // Ensures the format 'UIXXXXXXX'
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email_address: {
+        type: String,
+        required: true,
+        match: /.+\@.+\..+/, // Basic email validation regex
+    },
+    phone_number: {
+        type: String,
+        required: true,
+        match: /^[89][0-9]{7}$/, // Ensures the phone number starts with 8 or 9 and is 8 digits long
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female"], // Ensures the value is either 'Male' or 'Female'
+    },
+    cafe: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cafe", // References the Cafe model
+        unique: true, // Ensures that an employee can only be linked to one cafe
+        required: true,
+    },
+    start_date: {
+        type: Date,
+        required: true, // Adds the start date when the employee begins working at the cafe
+    },
+});
+
+module.exports = mongoose.model("Employee", employeeSchema);
